1627ebeec2b889e0048f5dd192447d0a2c3dca16
andy <pauchokbk@gmail.com>
2023-01-26 11:22:25 +0200

    Initial commit

---
+++ src/main/java/com/andy/springdemodi/SpringDemoDiApplication.java
@@ -0,0 +1,13 @@
+package com.andy.springdemodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andy/springdemodi/SpringDemoDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.springdemodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
