8da9bdfdbcc41266a17a987dad65dac83b20335d
andy <pauchokbk@gmail.com>
2023-01-26 11:34:47 +0200

    Test DI with Controller

--- src/main/java/com/andy/springdemodi/SpringDemoDiApplication.java
+++ src/main/java/com/andy/springdemodi/SpringDemoDiApplication.java
@@ -1,13 +1,19 @@
 package com.andy.springdemodi;
 
+import com.andy.springdemodi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoDiApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringDemoDiApplication.class, args);
+
+		MyController myController = context.getBean("myController", MyController.class);
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
